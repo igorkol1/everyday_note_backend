4fa44ceb5efee348946252a3c75fec8afc8dfd2d
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2021-05-01 08:20:14 +0200

    Initial commit

---
+++ src/main/kotlin/com/igorkol/everyday_note_backend/EverydayNoteBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.igorkol.everyday_note_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EverydayNoteBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<EverydayNoteBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/igorkol/everyday_note_backend/EverydayNoteBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.igorkol.everyday_note_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EverydayNoteBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
